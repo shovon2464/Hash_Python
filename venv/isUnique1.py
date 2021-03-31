ec9220508a837f46143bf0efdf6dbc3d08404203
shovon2464 <shovon2464@gmail.com>
2021-03-31 19:53:25 +0600

    problem 1 is solved using python

---
+++ venv/isUnique1.py
@@ -0,0 +1,14 @@
+def isUniqueChars(str):
+    if(len(str)>128):
+        return False
+    char_set = [False]*128
+
+    for i in range(0,len(str)):
+        char_pos = ord(str[i])
+        if(char_set[char_pos] == True):
+            return False
+        else:
+            char_set[char_pos] = True
+    return True
+
+print(isUniqueChars('shovn'))
